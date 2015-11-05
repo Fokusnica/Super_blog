915f2462432ce7a94434b5c3ae9f103a753f5e58
Derevianko Liubov <dereviankoliubov@gmail.com>
2015-11-05 13:50:32 +0200

    added comments contoroller and some views

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,12 @@
 class Comment < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :commentable, :polymorphic => true
-  has_many :comments, :as => :commentable
+	belongs_to :user
+	belongs_to :commentable, :polymorphic => true
+	has_many :comments, :as => :commentable
+
+	def post
+		return @post if defined?(@post)
+		@post = commentable.is_a?(Post) ? commentable : commentable.post
+ # if “commentable” (the parent object) is a post, return that. Otherwise, call this same method on the parent, which will in turn check if *it* is a Post, and so on.
+	end
+
 end
