6da3e81f6d9f66bb67aa217f88557a3089377480
Derevianko Liubov <dereviankoliubov@gmail.com>
2015-11-06 17:11:42 +0200

    add paperclip and users avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
+	has_attached_file :avatar, :styles => { :medium => "300*300", :thumb => "100*100", :com => "64*64" }
 	has_many :posts
-has_secure_password
+	has_secure_password
 
-validates :email, presence: true, uniqueness: true
-validates :name, presence: :true	
+	validates :email, presence: true, uniqueness: true
+	validates :name, presence: :true	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105152537) do
+ActiveRecord::Schema.define(version: 20151106141856) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "title"
@@ -39,9 +39,13 @@ ActiveRecord::Schema.define(version: 20151105152537) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "password_digest"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
