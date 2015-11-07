2acd8b5fe5ec6ad4cb2b888c5e554fa24755d5b3
Derevianko Liubov <dereviankoliubov@gmail.com>
2015-11-07 18:02:52 +0200

    comment model fixes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
-	belongs_to :user, :dependent => :destroy
-	belongs_to :post, :dependent => :destroy
+	belongs_to :user
+	belongs_to :post
 	belongs_to :comment, :dependent => :destroy
 	belongs_to :commentable, :polymorphic => true
 	has_many :comments, :as => :commentable, :dependent => :destroy
